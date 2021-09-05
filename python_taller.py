165c8728ff0376b296712f51ac87f9913a1be614
oscar p <oscarperezm16@hotmail.com>
2021-09-04 21:19:31 -0500

    pulsasiones_cardiacas

--- python_taller.py
+++ python_taller.py
@@ -4,9 +4,17 @@ Created on Sat Sep  4 19:33:17 2021
 
 @author: oscar perez
 """
+#cardiaco
+edad=55;
+numero= (200 - edad)/10;
+print ('numero de pulsaciones'(numero));
+
+#masa
 presion=20;
 volumen=10;
 temperatura=3;
 masa= (presion * volumen) / (0.37 * (temperatura + 460));
 
 print (masa);
+
+
