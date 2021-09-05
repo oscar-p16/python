c8318ccc5970cb5ace71741ac1ef39a06ce1a82f
oscar p <oscarperezm16@hotmail.com>
2021-09-04 21:31:05 -0500

    porcentaje

--- python_taller.py
+++ python_taller.py
@@ -7,7 +7,7 @@ Created on Sat Sep  4 19:33:17 2021
 #cardiaco
 edad=55;
 numero= (200 - edad)/10;
-print ('numero de pulsaciones'(numero));
+print ((numero));
 
 #masa
 presion=20;
@@ -17,4 +17,18 @@ masa= (presion * volumen) / (0.37 * (temperatura + 460));
 
 print (masa);
 
+#porcentaje
+persona1=10000;
+persona2=20000;
+persona3=30000;
+suma= persona1+persona2+persona3;
+print ('la invercion total es', suma);
 
+porp1= (persona1/suma)*100;
+print ('porcentaje de la primera persona es', porp1, "%");
+
+porp2= (persona2/suma)*100;
+print ('porcentaje de la segunda persona es', porp2, '%' );
+
+porp3= (persona3/suma)*100;
+print ('porcentaje de la tercera persona es de', porp3, '%');
